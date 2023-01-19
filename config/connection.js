e56f8402f2f4a6b5d476719642354fc09c63c824
Daniel Aguilera <dan.aguilera97@gmail.com>
2023-01-18 19:02:55 -0700

    added the rest that was needed for assignment

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,18 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// var http=require('http');
+
+// var server=http.createServer(function(req,res){
+//     res.end('test');
+// });
+
+// server.on('listening',function(){
+//     console.log('ok, server is running');
+// });
+
+// server.listen(3001);
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
+// The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
       include: [{ model: Product }]
@@ -14,6 +17,8 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
@@ -29,6 +34,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -38,7 +44,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-
+  // update a category by its `id` value
   await Category.update(req.body, {
     where: {
       id: req.params.id,
@@ -54,7 +60,7 @@ router.put('/:id', async (req, res) => {
 
 
 router.delete('/:id', async (req, res) => {
-
+  // delete a category by its id value
   const categoryData = Category.destroy({
     where: {
       id: req.params.id,
