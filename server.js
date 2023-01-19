f4a4e56bc4394e24f2652e6f31029baac6846d69
Daniel Aguilera <dan.aguilera97@gmail.com>
2023-01-19 16:28:00 -0700

    added neccesary code to assignment and was trouble shooting why insomnia wasnt connecting to my port assignment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3298;
+const PORT = process.env.PORT || 3002
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
